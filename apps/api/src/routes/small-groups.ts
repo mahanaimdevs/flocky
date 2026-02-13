844e55919e9e4f0cda3019a3358bb8c085f4124d
Peter An <anpeteran2@gmail.com>
2026-02-13 14:03:53 +1300

    chore: update tsconfig and import paths

--- apps/api/src/routes/small-groups.ts
+++ apps/api/src/routes/small-groups.ts
@@ -1,6 +1,6 @@
 import { Router, type Router as RouterType } from "express";
 
-import * as smallGroupsController from "../controllers/small-groups";
+import * as smallGroupsController from "../controllers/small-groups.js";
 
 const router: RouterType = Router();
 
